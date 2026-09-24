b96bbd9d1465fd940dce61ab314bebd29296eef5
webmo-lalday <lalday@web-mo.com>
2026-09-24 11:38:07 -0700

    fix nav widescreen

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,9 @@ export default function Home() {
     return (
         <article>
             <h1>Site Template</h1>
+            <div>
+                <h2>Hero Section</h2>
+            </div>
         </article>
     )
 }
